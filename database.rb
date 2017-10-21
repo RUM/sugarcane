b3d4fe793133918db86218d7143dadfa18ac5fa6
svexican <svexican@gmail.com>
2017-10-21 20:45:56 +0200

    latest releases section (part II)

--- database.rb
+++ database.rb
@@ -22,8 +22,8 @@ $db_releases_all = -> {
   db("/releases?select=#{release_attrs},number&order=date.desc")
 }
 
-$db_releases_latest = -> {
-  db("/releases?select=#{release_attrs}&online=eq.true&order=date.desc&limit=3")
+$db_releases_latest = -> (i) {
+  db("/releases?select=#{release_attrs}&online=eq.true&order=date.desc&limit=#{i}")
 }
 
 $db_releases = -> {
