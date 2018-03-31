14795f89b0c6073b63192f247e3a56af09eea90a
svexican <svexican@gmail.com>
2018-03-31 08:28:01 +0200

    KISSed expandables

--- public/javascripts/expandable.js
+++ public/javascripts/expandable.js
@@ -3,7 +3,8 @@ _expandables = function() {
 
   expandables.forEach(function(e) {
     var o = document.createElement('a');
-    o.href = '#';
+    var i = e.querySelector('img');
+
     var sty = `
 font-size: medium;
 background-color: #e3655a;
@@ -14,69 +15,21 @@ top: 1.7em;
 right: 0;
 display: block;
 `;
-    o.innerText = 'Expandir imagen';
-    o.style = sty;
-
-    var i = e.querySelector('img');
-
-    var a = (event) => {
-      event.preventDefault();
-      event.stopPropagation();
-
-      // the strong assumption here is that the styles attributes were
-      // empty in the first place... and ugly waiting to happen.
-
-      i.style = null;
-      e.style = null;
-      o.style = sty;
-
-      i.removeEventListener('click', a);
-      o.addEventListener('click', b);
-    };
 
     var b = (event) => {
       event.preventDefault();
       event.stopPropagation();
 
-      e.style = `
-position: absolute;
-padding: 1em !important;
-margin: 0;
-z-index: 10;
-text-align: center;
-top: 0;
-left: 0;
-background-color: rgba(0, 0, 0, 0.7);
-`;
-
-      o.style["display"] = "none";
-
-      i.style["margin"] = "auto";
-      i.style["width"]  = "auto";
-      i.style["height"] = "auto";
-
-      var oo = document.createElement('a');
-      oo.innerText = "Puedes regresar dando click en la imagen";
-      oo.style = `
-background-color: #e3655a;
-padding: 0.5em 1em;
-color: #f1f1f1;
-position: fixed;
-top: 1em;
-right: 1em;
-z-index: 1000;
-font-family: Cormorant, serif;
-font-size: 2em;
-`;
-
-      oo.addEventListener('click', () => oo.remove());
-      document.body.appendChild(oo);
-      setTimeout(() => { if (oo) oo.remove() }, 5000);
-
-      e.addEventListener('click', a);
+      window.open(i.src, '_blank');
     };
 
-    o.addEventListener('click', b);
+    o.href = i.src;
+    o.target = "_blank";
+    o.innerText = 'Expandir imagen';
+    o.style = sty;
+
+    i.style.cursor = 'pointer';
+    i.addEventListener('click', b);
 
     e.prepend(o);
   });
